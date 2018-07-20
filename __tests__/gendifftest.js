56a28134157a82a2b68402cc90ebc19c49fe28c4
Pavel Cherepanov <pavel@cherepanov.pro>
2018-07-20 21:13:11 +0300

    test (package) add a function that generates a path, but does not select it from the list

--- __tests__/gendifftest.js
+++ __tests__/gendifftest.js
@@ -1,59 +1,35 @@
 import fs from 'fs';
+import path, { extname } from 'path';
 import gendiff from '../src';
 
 const encoding = 'utf-8';
+const fixturesDir = '__tests__/__fixtures__/';
 
-const fixturesPaths = {
-  beforeFlatJson: '__tests__/__fixtures__/JSON/flat.before.json',
-  afterFlatJson: '__tests__/__fixtures__/JSON/flat.after.json',
-  beforeFlatYaml: '__tests__/__fixtures__/YAML/flat.before.yml',
-  afterFlatYaml: '__tests__/__fixtures__/YAML/flat.after.yml',
-  beforeFlatIni: '__tests__/__fixtures__/INI/flat.before.ini',
-  afterFLatIni: '__tests__/__fixtures__/INI/flat.after.ini',
-
-  beforeRecursiveJson: '__tests__/__fixtures__/JSON/recursive.before.json',
-  afterRecursiveJson: '__tests__/__fixtures__/JSON/recursive.after.json',
-  beforeRecursiveYaml: '__tests__/__fixtures__/YAML/recursive.before.yml',
-  afterRecursiveYaml: '__tests__/__fixtures__/YAML/recursive.after.yml',
-  beforeRecursiveIni: '__tests__/__fixtures__/INI/recursive.before.ini',
-  afterRecursiveIni: '__tests__/__fixtures__/INI/recursive.after.ini',
-
-  objRenderOfFlatObject: '__tests__/__fixtures__/flat.expected',
-  objRenderOfRecursiveJson: '__tests__/__fixtures__/JSON/recursive.json.expected',
-  objRenderOfRecursiveYaml: '__tests__/__fixtures__/YAML/recursive.yaml.expected',
-  objRenderOfRecursiveIni: '__tests__/__fixtures__/INI/recursive.ini.expected',
-  plainRenderOfFlatObject: '__tests__/__fixtures__/flat.plaintext.expected',
-  plainRenderOfRecursiveJson: '__tests__/__fixtures__/JSON/recursive.plaintext.expected',
-  plainRenderOfRecursiveYaml: '__tests__/__fixtures__/YAML/recursive.plaintext.expected',
-  plainRenderOfRecursiveIni: '__tests__/__fixtures__/INI/recursive.plaintext.expected',
-  jsonRenderOfRecursiveJson: '__tests__/__fixtures__/JSON/recursive.jsonFormat.expected',
-  jsonRenderOfRecursiveYaml: '__tests__/__fixtures__/YAML/recursive.jsonFormat.expected',
-  jsonRenderOfRecursiveIni: '__tests__/__fixtures__/INI/recursive.jsonFormat.expected',
-};
-
-const getFixturePath = name => fixturesPaths[name];
-
+const makeDirName = filename => extname(filename).slice(1);
+const getFixturePath = name => path.join(fixturesDir, makeDirName(name), name);
 
 describe('Comparison for the difference between flat files', () => {
-  const expectedRender = fs.readFileSync(getFixturePath('objRenderOfFlatObject'), encoding);
+  const expectedRender = fs.readFileSync(
+    getFixturePath('objRenderOfFlat.expected'), encoding,
+  );
 
   it('Compare two JSON flat files', () => {
     const actual = gendiff(
-      getFixturePath('beforeFlatJson'), getFixturePath('afterFlatJson'),
+      getFixturePath('beforeFlat.json'), getFixturePath('afterFlat.json'),
     );
     expect(actual).toBe(expectedRender);
   });
 
   it('Compare two YAML flat files', () => {
     const actual = gendiff(
-      getFixturePath('beforeFlatYaml'), getFixturePath('afterFlatYaml'),
+      getFixturePath('beforeFlat.yml'), getFixturePath('afterFlat.yml'),
     );
     expect(actual).toBe(expectedRender);
   });
 
   it('Compare two INI flat files', () => {
     const actual = gendiff(
-      getFixturePath('beforeFlatIni'), getFixturePath('afterFLatIni'),
+      getFixturePath('beforeFlat.ini'), getFixturePath('afterFlat.ini'),
     );
     expect(actual).toBe(expectedRender);
   });
@@ -61,33 +37,33 @@ describe('Comparison for the difference between flat files', () => {
 
 describe('Comparison for the difference between recursive files', () => {
   const expectedJsonRender = fs.readFileSync(
-    getFixturePath('objRenderOfRecursiveJson'), encoding,
+    getFixturePath('objRenderOfRecursiveJson.expected'), encoding,
   );
 
   const expectedYamlRender = fs.readFileSync(
-    getFixturePath('objRenderOfRecursiveYaml'), encoding,
+    getFixturePath('objRenderOfRecursiveYaml.expected'), encoding,
   );
   const expectedIniRender = fs.readFileSync(
-    getFixturePath('objRenderOfRecursiveIni'), encoding,
+    getFixturePath('objRenderOfRecursiveIni.expected'), encoding,
   );
 
   it('Compare two JSON recursive files', () => {
     const actual = gendiff(
-      getFixturePath('beforeRecursiveJson'), getFixturePath('afterRecursiveJson'),
+      getFixturePath('beforeRecursive.json'), getFixturePath('afterRecursive.json'),
     );
     expect(actual).toBe(expectedJsonRender);
   });
 
   it('Compare two YAML recursive files', () => {
     const actual = gendiff(
-      getFixturePath('beforeRecursiveYaml'), getFixturePath('afterRecursiveYaml'),
+      getFixturePath('beforeRecursive.yml'), getFixturePath('afterRecursive.yml'),
     );
     expect(actual).toBe(expectedYamlRender);
   });
 
   it('Compare two INI recursive files', () => {
     const actual = gendiff(
-      getFixturePath('beforeRecursiveIni'), getFixturePath('afterRecursiveIni'),
+      getFixturePath('beforeRecursive.ini'), getFixturePath('afterRecursive.ini'),
     );
     expect(actual).toBe(expectedIniRender);
   });
@@ -95,25 +71,27 @@ describe('Comparison for the difference between recursive files', () => {
 
 
 describe('Comparison of the difference between flat files in plaintext format', () => {
-  const expectedRender = fs.readFileSync(getFixturePath('plainRenderOfFlatObject'), encoding);
+  const expectedRender = fs.readFileSync(
+    getFixturePath('plainRenderOfFlat.expected'), encoding,
+  );
 
   it('Compare two JSON flat files', () => {
     const actual = gendiff(
-      getFixturePath('beforeFlatJson'), getFixturePath('afterFlatJson'), 'plain',
+      getFixturePath('beforeFlat.json'), getFixturePath('afterFlat.json'), 'plain',
     );
     expect(actual).toBe(expectedRender);
   });
 
   it('Compare two YAML flat files', () => {
     const actual = gendiff(
-      getFixturePath('beforeFlatYaml'), getFixturePath('afterFlatYaml'), 'plain',
+      getFixturePath('beforeFlat.yml'), getFixturePath('afterFlat.yml'), 'plain',
     );
     expect(actual).toBe(expectedRender);
   });
 
   it('Compare two INI flat files', () => {
     const actual = gendiff(
-      getFixturePath('beforeFlatIni'), getFixturePath('afterFLatIni'), 'plain',
+      getFixturePath('beforeFlat.ini'), getFixturePath('afterFlat.ini'), 'plain',
     );
     expect(actual).toBe(expectedRender);
   });
@@ -122,32 +100,32 @@ describe('Comparison of the difference between flat files in plaintext format', 
 
 describe('Comparison of the difference between recursive files in plaintext format', () => {
   const expectedJsonRender = fs.readFileSync(
-    getFixturePath('plainRenderOfRecursiveJson'), encoding,
+    getFixturePath('plainRenderOfRecursiveJson.expected'), encoding,
   );
   const expectedYamlRender = fs.readFileSync(
-    getFixturePath('plainRenderOfRecursiveYaml'), encoding,
+    getFixturePath('plainRenderOfRecursiveYaml.expected'), encoding,
   );
   const expectedIniRender = fs.readFileSync(
-    getFixturePath('plainRenderOfRecursiveIni'), encoding,
+    getFixturePath('plainRenderOfRecursiveIni.expected'), encoding,
   );
 
   it('Compare two JSON recursive files', () => {
     const actual = gendiff(
-      getFixturePath('beforeRecursiveJson'), getFixturePath('afterRecursiveJson'), 'plain',
+      getFixturePath('beforeRecursive.json'), getFixturePath('afterRecursive.json'), 'plain',
     );
     expect(actual).toBe(expectedJsonRender);
   });
 
   it('Compare two YAML recursive files', () => {
     const actual = gendiff(
-      getFixturePath('beforeRecursiveYaml'), getFixturePath('afterRecursiveYaml'), 'plain',
+      getFixturePath('beforeRecursive.yml'), getFixturePath('afterRecursive.yml'), 'plain',
     );
     expect(actual).toBe(expectedYamlRender);
   });
 
   it('Compare two INI recursive files', () => {
     const actual = gendiff(
-      getFixturePath('beforeRecursiveIni'), getFixturePath('afterRecursiveIni'), 'plain',
+      getFixturePath('beforeRecursive.ini'), getFixturePath('afterRecursive.ini'), 'plain',
     );
     expect(actual).toBe(expectedIniRender);
   });
@@ -156,32 +134,32 @@ describe('Comparison of the difference between recursive files in plaintext form
 
 describe('Comparison of the difference between recursive files in JSON format', () => {
   const expectedJsonRender = fs.readFileSync(
-    getFixturePath('jsonRenderOfRecursiveJson'), encoding,
+    getFixturePath('jsonRenderOfRecursiveJson.expected'), encoding,
   );
   const expectedYamlRender = fs.readFileSync(
-    getFixturePath('jsonRenderOfRecursiveYaml'), encoding,
+    getFixturePath('jsonRenderOfRecursiveYaml.expected'), encoding,
   );
   const expectedIniRender = fs.readFileSync(
-    getFixturePath('jsonRenderOfRecursiveIni'), encoding,
+    getFixturePath('jsonRenderOfRecursiveIni.expected'), encoding,
   );
 
   it('Compare two JSON recursive files', () => {
     const actual = gendiff(
-      getFixturePath('beforeRecursiveJson'), getFixturePath('afterRecursiveJson'), 'json',
+      getFixturePath('beforeRecursive.json'), getFixturePath('afterRecursive.json'), 'json',
     );
     expect(actual).toBe(expectedJsonRender);
   });
 
   it('Compare two YAML recursive files', () => {
     const actual = gendiff(
-      getFixturePath('beforeRecursiveYaml'), getFixturePath('afterRecursiveYaml'), 'json',
+      getFixturePath('beforeRecursive.yml'), getFixturePath('afterRecursive.yml'), 'json',
     );
     expect(actual).toBe(expectedYamlRender);
   });
 
   it('Compare two INI recursive files', () => {
     const actual = gendiff(
-      getFixturePath('beforeRecursiveIni'), getFixturePath('afterRecursiveIni'), 'json',
+      getFixturePath('beforeRecursive.ini'), getFixturePath('afterRecursive.ini'), 'json',
     );
     expect(actual).toBe(expectedIniRender);
   });
